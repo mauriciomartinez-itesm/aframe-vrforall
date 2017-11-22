d45068650d5ece6becb12fede74d8da442069081
Glitch (aframe-taptowalk) <none>
2017-11-22 21:52:36 +0000

    🐑📉 Updated with Glitch

--- public/wasd-controls-custom.js
+++ public/wasd-controls-custom.js
@@ -8,32 +8,32 @@ AFRAME.registerComponent('wasd-controls-custom', {
           document.addEventListener('keydown', function (e){
              switch(e.keyCode){
                case 87:
-                 var w = true;
+                 w = true;
                  break;
                case 65:
-                 var a = true;
+                 a = true;
                  break;
                case 83:
-                 var s = true;
+                 s = true;
                  break;
                case 68:
-                 var d = true;
+                 d = true;
                  break;
             }
           });
          document.addEventListener('keyup', function (e) {
              switch(e.keyCode){
                case 87:
-                 var w = false;
+                 w = false;
                  break;
                case 65:
-                 var a = false;
+                 a = false;
                  break;
                case 83:
-                 var s = false;
+                 s = false;
                  break;
                case 68:
-                 var d = false;
+                 d = false;
                  break;
             }
          });
@@ -41,24 +41,41 @@ AFRAME.registerComponent('wasd-controls-custom', {
 		
   //EN EL TICK (SUCEDE CADA MOMENTO, ES COMO EL UPDATE O ENTERFRAME) HAGO EL CÁLCULO TRIGONOMÉTRICO PARA QUE EL JUGADOR AVANCE HACIA DONDE ESTÁ VIENDO
 		tick: function(time, timeDelta){
-      //SI EL BOTÓN DEL MOUSE ESTÁ ABAJO
-		  	if(w){
           
-          //DECLARO MIS VARIABLES (QUE INCLUYEN LA ENTIDAD DE CÁMARA, POSICIONES EN X,Y,Z Y ROTACIÓN EN Y)
-          var camara = document.querySelector('#eyes');
-          var player = document.querySelector('#player');
-          var px =player.getAttribute("position").x;
-          var py =player.getAttribute("position").y;
-          var pz =player.getAttribute("position").z;
-          var ry =camara.getAttribute("rotation").y;
-
-          //PARA SABER A DONDE ESTÁ VOLTEANDO A VER Y AÑADIRLE VELOCIDAD HACIA ESE LUGAR, NECESITO USAR EL SENO Y COSENO DE LOS ÁNGULOS
-          var sy = Math.sin(ry*Math.PI/180);
-          var cy = Math.cos(ry*Math.PI/180);
-
-          //lE AÑADO LOS ATRIBUTOS PARA QUE PUEDA CAMINAR HACIA DONDE SE EL PIDE (MUEVO LA CÁMARA EN X y Z CON LA VELOCIDAD CALCULADA DESDE EL SENO Y COSENO)
-          player.setAttribute("position", {x:px-(sy*speed),y:py,z:pz-(cy*speed)});
-			  }
-		  }  
+      //DECLARO MIS VARIABLES (QUE INCLUYEN LA ENTIDAD DE CÁMARA, POSICIONES EN X,Y,Z Y ROTACIÓN EN Y)
+      var camara = document.querySelector('#eyes');
+      var player = document.querySelector('#player');
+      var px =player.getAttribute("position").x;
+      var py =player.getAttribute("position").y;
+      var pz =player.getAttribute("position").z;
+      var ry =camara.getAttribute("rotation").y;
+      
+      //PARA SABER A DONDE ESTÁ VOLTEANDO A VER Y AÑADIRLE VELOCIDAD HACIA ESE LUGAR, NECESITO USAR EL SENO Y COSENO DE LOS ÁNGULOS
+      var sy = Math.sin(ry*Math.PI/180);
+      var cy = Math.cos(ry*Math.PI/180);
+      var ty = Math.tan(ry*Math.PI/180);
+            
+      //SI LE ESTOY PICANDO A LA W, A, S o D
+      //lE AÑADO LOS ATRIBUTOS PARA QUE PUEDA CAMINAR HACIA DONDE SE EL PIDE (MUEVO LA CÁMARA EN X y Z CON LA VELOCIDAD CALCULADA DESDE EL SENO Y COSENO)
+      if(w){
+        mix -= sy*speed;
+        miz -= cy*speed;
+      }
+      if(a){
+        mix -= cy*speed;
+        miz += sy*speed;
+      }
+      if(s){
+        mix += sy*speed;
+        miz += cy*speed;
+      }
+      if(d){
+        mix += cy*speed;
+        miz -= sy*speed;
+      }
+      
+      player.setAttribute("position",{x:mix,y:py,z:miz});
+        
+		}
 		  
 });
