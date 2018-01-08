e855f7f7c4ca0b36321c6b4b9cd0ba58c3213158
Glitch (aframe-taptowalk) <none>
2018-01-08 15:52:12 +0000

    🎊😂 Updated with Glitch

--- public/teleport-sounds.js
+++ public/teleport-sounds.js
@@ -5,12 +5,12 @@ window.addEventListener('teleported', endTeleportSound);
 
 function startTeleportSound(){
   var audioEl4 = document.querySelector("#question_sfx");
-  audioEl4.components.sound.playSound();
+ // audioEl4.components.sound.playSound();
 }
 
 function endTeleportSound(event){
    
   var audioEl4 = document.querySelector("#teleport_sfx");
-  audioEl4.components.sound.playSound();
+  //audioEl4.components.sound.playSound();
   
 }
